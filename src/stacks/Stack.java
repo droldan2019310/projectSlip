6efa4001956c208e0d3ba27a56344de47515c121
jorgelopez <lop221038@uvg.edu.gt>
2023-03-09 09:47:41 -0600

    Se termino la clase Stack

--- src/stacks/Stack.java
+++ src/stacks/Stack.java
@@ -1,5 +1,33 @@
 package stacks;
+import java.util.Vector;
+public class Stack <T>{
 
-public class Stack {
-    
+    //propiedades
+    private Vector<T> vector;
+    public Stack (){
+        vector = new Vector<T>();
+    }
+    public void push(T value) {
+        vector.add(value);
+    }
+    public T pull() {
+        T value;
+        if (isEmpty()) //Si la pila está vacía
+            value = null;
+        else{ //Si se tiene algún elemento
+            value = peek();
+            vector.remove(vector.get(count()-1));
+        }
+        return value;
+    }
+    public T peek() {
+        return vector.lastElement();
+    }
+    public int count() {
+        return vector.size();
+    }
+    public boolean isEmpty() {
+        return vector.isEmpty();
+    }
+
 }
