ca6f63453b299f79ae01b29df83bf4861a0d48de
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-03-09 09:51:57 -0600

    implement interface keyword

---
+++ src/interfaces/IKeyWords.java
@@ -0,0 +1,36 @@
+package interfaces;
+
+public interface IKeyWords {
+    
+    public void commentNestable();
+
+    public void commentEndLine();
+
+    public void greaterThan();
+
+    public void lowerThan();
+
+    public void greaterIqualThan();
+
+    public void lowerIqualThan();
+
+    public void defun();
+
+    public void equal();
+
+    public void equalP();
+
+    public void setQ();
+
+    public void setF();
+
+    public void set();
+
+    public void cond();
+
+    public void loop();
+
+    
+
+
+}
