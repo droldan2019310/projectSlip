9a4dbf2d33e0f196efbdccc2936e0ba6dc2e81a0
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-03-09 09:28:30 -0600

    implement map

--- src/interfaces/IMap.java
+++ src/interfaces/IMap.java
@@ -1,5 +1,12 @@
 package interfaces;
 
-public interface IMap {
+public interface IMap<V> {
     
+    public boolean add(String key, V value);
+
+    public V getValue(String key);
+
+    public void remove(String key);
+
+
 }
--- src/maps/HashMapClass.java
+++ src/maps/HashMapClass.java
@@ -1,5 +1,36 @@
 package maps;
 
-public class HashMapClass {
+import java.util.HashMap;
+
+import interfaces.IMap;
+
+public class HashMapClass<V> implements IMap<V> {
+    HashMap<String,V> variables = new HashMap<String, V>(); 
+    
+    
+    
+    @Override
+    public boolean add(String key, V value) {
+        // TODO Auto-generated method stub
+        try {
+            variables.put(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public V getValue(String key) {
+        // TODO Auto-generated method stub
+        return variables.get(key);
+    }
+
+    @Override
+    public void remove(String key) {
+        // TODO Auto-generated method stub
+        variables.remove(key);
+    }
     
 }
