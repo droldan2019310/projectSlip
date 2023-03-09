b38b5cd3eb1b48e85355f685b6d10440061512ff
droldan-2019310 <droldan-2019310@kinal.edu.gt>
2023-03-09 09:41:50 -0600

    function read file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App app =  new App();
+
+        app.start();
     }
+
+
+    public void start(){
+        readFile();
+    }
+
+
+
+    /**
+     * read file in assets/file.txt
+     */
+    public void readFile(){
+        File file = new File("src/program.txt");
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+ 
+        // Creating an object of BufferedReader class
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            
+            
+            String data = br.readLine();
+            
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e);
+            System.out.println("ARCHIVO NO ENCONTRADO");
+        }
+    }
+
+  
+
 }
