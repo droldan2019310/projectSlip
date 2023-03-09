347b180943eed33ecad703b1dc3a08112765d4b7
jorgelopez <lop221038@uvg.edu.gt>
2023-03-09 10:47:02 -0600

    clase Stack finalizada

--- src/stacks/Stack.java
+++ src/stacks/Stack.java
@@ -1,27 +1,32 @@
 package stacks;
 import interfaces.Istack;
 import java.util.EmptyStackException;
+import java.util.LinkedList;
 
 public class Stack <T> implements Istack<T> {
     //propiedades
-    Stack<T> stack = new Stack<>();
+    private LinkedList <T> linkedList = new LinkedList();
+
     @Override
-    public void push(T item) {stack.push(item);
+    public void push(T item) {
+         linkedList.push(item);
+
     }
     @Override
     public T pop() throws EmptyStackException {
-        return stack.pop();
+        return linkedList.pop();
     }
     @Override
     public T peek() throws EmptyStackException {
-        return stack.peek();
+        return linkedList.getFirst();
     }
     @Override
     public boolean empty() {
-        return stack.empty();
+        return linkedList.isEmpty();
     }
     @Override
     public int size() {
-        return stack.size();
+        return linkedList.size();
     }
+
 }
