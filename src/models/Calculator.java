99afb81b398e992e8f5c824c2b295d871ee2d602
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-03-10 17:28:42 -0600

    Modificacion de Clase Calculadora
    Cambio de clase calculadora para leer datos en prefix

--- src/models/Calculator.java
+++ src/models/Calculator.java
@@ -1,62 +1,53 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
 import java.util.Stack;
 
-package models;
-
 public class Calculator {
-    public static void main(String[] args){
-        String dataName = "datos.txt";
-        try(BufferedReader reader = new BufferedReader(new FileReader(dataName))){
-            String line;
-            while ((line = reader.readLine()) != null){
-                int result = evaluatePostfix(line);
-                System.out.println(line +"="+result);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la operación en formato prefix ");
+
+        String input = scanner.nextLine();
+        String[] tokens = input.split(" ");
+
+        Stack<Double> stack = new Stack<>();
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            String token = tokens[i];
+            if (isNumber(token)) {
+                stack.push(Double.parseDouble(token));
+            } else {
+                double num1 = stack.pop();
+                double num2 = stack.pop();
+                double resultado = 0;
+                switch (token) {
+                    case "+":
+                        resultado = num1 + num2;
+                        break;
+                    case "-":
+                        resultado = num1 - num2;
+                        break;
+                    case "*":
+                        resultado = num1 * num2;
+                        break;
+                    case "/":
+                        resultado = num1 / num2;
+                        break;
+                    default:
+                        System.out.println("Operador inválido., vuelava a ingresar el dato");
+                        return;
+                }
+                stack.push(resultado);
             }
-
-        }catch(IOException e){
-            System.err.println("Error reading file: " + e.getMessage());
         }
-    }
-    
-    public static int evaluatePostfix(String expression){
-        Stack<Integer> stack = new Stack<>();
-        for(char c : expression.toCharArray()){
-            if(Character.isDigit(c)){
-                stack.push(c-'0');
-            } else if (c == '+' || c == '-' || c == '*' || c == '/'){
-                int operando2 = stack.pop();
-                int operando1 = stack.pop();
-                int result = applyOperando(c, operando1, operando2);
-                stack.push(result);
-            }
-        }
-        return stack.pop();
 
-
+        System.out.println("El resultado es: " + stack.pop());
     }
 
-    public static int applyOperando(char operator, int operando1, int operando2){
-        switch (operator){
-            case '+':
-                return operando1 + operando2;
-            case'-':
-                return operando1 - operando2;
-            case'*':
-                return operando1 * operando2;
-            case '/':
-                return operando1 / operando2;
-            default: 
-                throw new IllegalArgumentException("Uknow operator: " + operator);
-            
-            
-
-
-
+    private static boolean isNumber(String token) {
+        try {
+            Double.parseDouble(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
-
-
-
     }
 }
