83a4c55da0a01544e3e3b66a58ddf0543fed09c6
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-03-13 16:35:27 -0600

    Substract
    Substract method in App.Java

--- src/App.java
+++ src/App.java
@@ -134,7 +134,19 @@ public class App {
                         }
                         break;
                     case "-":
-                        
+                        try{
+                            double item1 = Double.parseDouble(stackToken.pop());
+                            double item2 = Double.parseDouble(stackToken.pop());
+                            double result = calculator.subtraction(item1, item2);
+                            if(!actualName.equals("")){
+                                doubleVariables.add(actualName, result);
+                                actualName = "";
+                            }
+
+                        }catch(NumberFormatException e){
+                            e.printStackTrace();
+                            throw new Exception("NOT A NUMBER");
+                        } 
                         break;
                     case "/":
                         
